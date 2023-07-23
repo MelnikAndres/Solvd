66381f6b21f0bd716cead6fdc4721580ebb3baf9
MelnikAndres <104472929+MelnikAndres@users.noreply.github.com>
2023-07-23 12:44:55 -0300

    Update: 2nd HW finished
    finished version of 2nd HW

--- secondLectureHomework.js
+++ secondLectureHomework.js
@@ -1,34 +1,142 @@
-//addValues: Accepts two arguments of any type and performs the appropriate addition operation based on the types of the arguments. 
-//The function should return the result of the addition. If the addition is not possible, it should throw an error.
+const isPrimitive = (value) =>{
+    return Object(value) !== value ;
+}
 
 const canBeAdded = (value) =>{
-    if(value === null){
+    if(value === null || value === undefined){
         return false;
     }
-    return Object(value.valueOf()) !== value.valueOf() ;
+    return isPrimitive(value) ;
 }
 
-const addValues = (arg1, arg2,arg3) =>{
-    console.log("Test " + arg3)
-    if(!canBeAdded(arg1) || !canBeAdded(arg2)){
+const addValues = (value1, value2) =>{
+    if(!canBeAdded(value1) || !canBeAdded(value2)){
+        throw new TypeError("Cannot add values of type " + typeof value1 + " and " + typeof value2)
     }
-    throw "TypeError: Cannot add values of type " + typeof arg1 + " and " + typeof arg2
+    return value1 + value2;
 }
 
-addValues(null,1,"null")
-addValues("1",1,"string")
-addValues("hola",1,"mixstring")
-addValues(true,1,"boolean")
-addValues(2,1,"number")
-//addValues(Symbol('foo'),"symbol")
-addValues(10n,1n,"bigint")
-addValues({},1,"object")
-addValues([],1,"array")
-function MyCustomType(n) {
-    this.number = n;
+const stringifyValue = (value) =>{
+    return isPrimitive(value) ? String(value) : JSON.stringify(value)
 }
-  
-MyCustomType.prototype.valueOf = function() {
-return this.number;
-};
-addValues(new MyCustomType(4),1,"custom type")
+
+const invertBoolean = (bValue) =>{
+    if(typeof bValue === 'boolean'){
+        return !bValue;
+    }
+    throw new TypeError("Cannot invert non-boolean type " + typeof bValue)
+}
+
+const convertToNumber = (value) =>{
+    let result;
+    if(typeof value === 'string'){
+        result = parseInt(value)
+    }else{
+        result = Number(value)
+    }
+    if(result === result){
+        return result
+    }
+    throw new TypeError("Cannot convert to number")
+}
+
+const coerceToType = (value, newType) =>{
+    const types = {string:String,number:Number, bigint:BigInt, boolean:Boolean, symbol:Symbol, object:Object}
+    if(types[newType]){
+        return types[newType](value)
+    }
+    throw TypeError("Unknown or invalid type " + newType)
+}
+
+class ConversionFollower {
+    constructor(value, name){
+        this.value = value
+        this.name = name
+    }
+    [Symbol.toPrimitive](hint) {
+        if(typeof this.value==='string' && hint==='default'){
+            hint='string'
+        }
+        if (hint === 'string') {
+            console.log("Converting "+ this.name + " to string with value:")
+            let result = this.value?this.value.toString():String(this.value)
+            console.log(result)
+            if(isPrimitive(result)){
+                return result
+            }
+            console.log("Converting primitive of "+ this.name + " to number with values:")
+            result = this.value?this.value.valueOf():this.value
+            console.log("Primitive value:")
+            console.log(result)
+            if(isPrimitive(result)){
+                console.log("Number value:")
+                console.log(Number(result))
+                return Number(result)
+            }
+            console.log("Primitive value is of type Object")
+            throw TypeError("Couldn't convert "+this.name+" to proper type")
+        }
+        if(hint==='number'){
+            console.log("Converting primitive of "+ this.name + " to number with values:")
+            let result = Number(this.value?this.value.valueOf():this.value)
+            console.log("Primitive value:")
+            console.log(result)
+            if(isPrimitive(result)){
+                console.log("Number value:")
+                console.log(result)
+                return result
+            }
+            console.log("Primitive value is of type Object")
+            console.log("Converting "+ this.name + " to string with value:")
+            result = this.value?this.value.toString():String(this.value)
+            console.log(result)
+            if(isPrimitive(result)){
+                return result
+            }
+            throw TypeError("Couldn't convert "+this.name+" to proper type")
+        }
+        console.log("Converting "+ this.name + " to primitive with value:")
+        let result = this.value?this.value.valueOf():this.value
+        console.log(result)
+        if(isPrimitive(result)){
+            return result
+        }
+        console.log("Primitive value is of type Object")
+        console.log("Converting "+ this.name + " to string with value:")
+        result = this.value?this.value.toString():String(this.value)
+        console.log(result)
+        if(isPrimitive(result)){
+            return result
+        }
+        throw TypeError("Couldn't convert "+this.name+" to proper type")
+        
+    }
+    valueOf(){
+        return this.value?this.value.valueOf():this.value
+    }
+    toString(){
+        return this.value?this.value.toString():String(this.value)
+    }
+}
+
+const OPERATIONS = 
+    {"+":(v1,v2)=>v1+v2,
+    "-":(v1,v2)=>v1-v2,
+    "*":(v1,v2)=>v1*v2,
+    "/":(v1,v2)=>v1/v2,
+    "%":(v1,v2)=>v1%v2,
+    "+ +":(v1,v2)=>v1+ +v2}
+    
+const followOperationConversions = (value1, value2, operation) =>{
+    
+    const firstValue = new ConversionFollower(value1,"First Value")
+    const secondValue = new ConversionFollower(value2,"Second Value")
+    if(OPERATIONS[operation]){
+        return OPERATIONS[operation](firstValue,secondValue)
+    }
+    throw Error("Unknown operation "+operation)
+}
+
+module.exports={
+    addValues,stringifyValue,invertBoolean,convertToNumber,coerceToType,followOperationConversions
+}
