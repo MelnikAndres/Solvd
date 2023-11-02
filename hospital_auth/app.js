966d2352e2042585880c42bf1e301492f199ff3a
MelnikAndres <104472929+MelnikAndres@users.noreply.github.com>
2023-11-02 19:14:07 -0300

    Delete: old folders

--- hospital_auth/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const jwtController = require('./http/JWTController')
-const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json())
-app.use('/', jwtController)
-app.listen('3030')
